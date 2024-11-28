8e69aaed254da1c6ef38ac7626a0cb4aaf2c52cc
Hammad Tanveer <142724583+Hammadtanveer@users.noreply.github.com>
2024-11-28 23:56:18 +0530

    student grade tracker

--- Student_Grade_Tracker.java
+++ Student_Grade_Tracker.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 
 public class Student_Grade_Tracker {
     public static void main(String[] args) {
-        System.out.println("enter the number of subject");
+        System.out.println("enter the number of students-:");
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        System.out.println("enter the marks of subjects");
-        float[] arr = new float[n];
-        for (int i = 0; i < n; i++) {
+        float[] arr = new float[n+1];
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Enter the Grade of student "+i+"-:");
             arr[i] = in.nextFloat();
         }
-        System.out.println("average marks-:"+avg(arr,n));
-        System.out.println("highest marks-:"+high(arr,n));
-        System.out.println("lowest marks-:"+low(arr,n));
+        System.out.println("Student Grade Summary:");
+        System.out.println("Average Grade -:"+avg(arr,n));
+        System.out.println("Highest Grade-:"+high(arr,n));
+        System.out.println("Lowest Grade-:"+low(arr,n));
     }
     static float avg(float[]arr,int n ){
         float sum=0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             sum=sum+arr[i];
         }
         float average=sum/n;
@@ -24,7 +25,7 @@ public class Student_Grade_Tracker {
     }
   static float high(float[] arr,int n){
         float highest = 0;
-      for (int i = 0; i < n; i++) {
+      for (int i = 1; i <=n; i++) {
           if (highest <arr[i]) {
               highest=arr[i];
           }
@@ -34,7 +35,7 @@ public class Student_Grade_Tracker {
   }
     static float low(float[] arr,int n){
         float lowest = 100;
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             if (lowest >arr[i]) {
                 lowest=arr[i];
             }
